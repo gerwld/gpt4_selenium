68276e8a8dfc7435da16847fc65cd28d4996f2a0
P Jaworski <pjaworski.dev@gmail.com>
2023-04-29 23:17:56 +0200

    Added links parsing

--- getlinks.py
+++ getlinks.py
@@ -1,6 +1,9 @@
+import os
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service as ChromeService
+
+BASE_URL = 'https://realpython.com'
 
 # Ініціалізація сервісу, опшинів хедлес бравзеру
 chromedriver = '/usr/local/bin/chromedriver'
@@ -10,7 +13,6 @@ service = ChromeService(executable_path = chromedriver);
 browser = webdriver.Chrome(service=service, options=options)
 
 # Переход на сторінку i отримання змісту
-BASE_URL = 'https://realpython.com'
 browser.get(BASE_URL + '/search')
 requiredHtml = browser.page_source
 
@@ -20,5 +22,15 @@ soup = BeautifulSoup(requiredHtml, 'html5lib')
 parentDiv = soup.find(id = "resultsArea");
 allLinksToArticles = parentDiv.select('.stretched-link')
 
+allLinksToArticlesInline = '';
 for a in allLinksToArticles:
-    print("Found the URL:", BASE_URL + a['href'])
+    allLinksToArticlesInline += (BASE_URL + a['href']+ '\n');
+    print(BASE_URL + a['href']);
+
+
+#створення нового файлу з усіма лінками
+filename = './posts/links.txt';
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+with open(filename, 'w+') as f:
+    f.write(allLinksToArticlesInline)
