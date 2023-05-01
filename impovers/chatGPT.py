58cbec840cebbe90d39e02919f99ab9c8b78b830
P Jaworski <pjaworski.dev@gmail.com>
2023-05-01 19:10:59 +0200

    Minor gptHandler improvements

--- impovers/chatGPT.py
+++ impovers/chatGPT.py
@@ -3,18 +3,25 @@ import time
 import random
 from impovers.handlers.gptHandler import Handler
 from helpers.createPost import createPost
-from global_context import POSTS_TO_MD
+from global_context import POSTS_TO_MD, PATH_TO_POSTS, MD_SET_DATE
 
-message = "make post not plagiarized, keep the structure, make it at least 1500 words length, answer just with post: \n"
+message = "make post not plagiarized, keep the structure, make it at least 1500 words length, put code in the code tag (only if exist), avoid using any tags except (<h1>, <p>,<code>, <ul>, <li>, <a>). answer just with post: \n"
 
 # отримання постів і прохід по ним, якщо існують
 if not os.path.exists(POSTS_TO_MD):
     print(f'POSTS_TO_MD: {POSTS_TO_MD} do not exist in improvers.$')
 else:
+    # перевірка наявності пройдених постів і фільтрація їх з основного масиву
     htmlPosts = os.listdir(os.path.dirname(POSTS_TO_MD))
+    donePosts = os.listdir(os.path.dirname(
+        PATH_TO_POSTS + "_gpt/" + MD_SET_DATE + "/"))
+    if len(donePosts):
+        print(
+            f'Founded {len(donePosts)} completed posts out of {len(htmlPosts)}. Skipping them...')
+        htmlPosts = list(filter(lambda x: x not in donePosts, htmlPosts))
 
+    # якщо є пости, запит на chatGPT через хандлер
     if htmlPosts and len(htmlPosts):
-        # запит на chatGPT через хандлер
         chatgpt = Handler('patryk.jaworski003@gmail.com', 'aboba12341234')
 
         for page in htmlPosts:
--- impovers/handlers/gptHandler.py
+++ impovers/handlers/gptHandler.py
@@ -1,90 +1,16 @@
-"""Class definition for ChatGPT Handler"""
-
 import time
 import undetected_chromedriver as uc
-
-# from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import selenium.common.exceptions as Exceptions
 
 
-class TalkingHeads:
-    """An interface for talking heads"""
-
-    def __init__(self, username: str, password: str, headless=False, head_count=2):
-        self.head_count = head_count
-        self.driver = Handler(username, password, headless)
-        for _ in range(head_count-1):
-            self.driver.browser.execute_script(
-                '''window.open("https://chat.openai.com/chat","_blank");''')
-            time.sleep(1)
-
-        self.head_responses = [[] for _ in range(head_count)]
-
-    def switch_to_tab(self, idx: int = 0):
-        "Switch to tab"
-        windows = self.driver.browser.window_handles
-        if idx > len(windows):
-            print(f"There is no tab with index {idx}")
-            return
-        self.driver.browser.switch_to.window(windows[idx])
-
-    def interact(self, head_number, question):
-        """interact with the given head"""
-        self.switch_to_tab(head_number)
-        response = self.driver.interact(question)
-        return response
-
-    def reset_thread(self, head_number):
-        """reset heads for the given number"""
-        self.heads[head_number].reset_thread()
-
-    def reset_all_threads(self):
-        """reset heads for the given number"""
-        for head in range(self.head_count):
-            self.switch_to_tab(head)
-            head.reset_thread()
-
-    def start_conversation(self, text_1: str, text_2: str, use_response_1: bool = True):
-        """Starts a conversation between two heads"""
-        assert len(
-            self.heads) >= 2, "At least 2 heads is neccessary for a conversation"
-
-        f_response = self.interact(0, text_1)
-        text_2 = text_2 + f_response if use_response_1 else text_2
-        s_response = self.interact(1, text_2)
-
-        self.head_responses[0].append(f_response)
-        self.head_responses[1].append(s_response)
-
-        return f_response, s_response
-
-    def continue_conversation(self, text_1: str = None, text_2: str = None):
-        """Make another round of conversation.
-        If text_1 or text_2 is given, the response is not used"""
-        text_1 = text_1 or self.head_responses[1][-1]
-
-        f_response = self.interact(0, text_1)
-        text_2 = text_2 or f_response
-
-        s_response = self.interact(1, text_2)
-
-        self.head_responses[0].append(f_response)
-        self.head_responses[1].append(s_response)
-        return f_response, s_response
-
-
 class Handler:
-    """Handler class to interact with ChatGPT"""
-
     login_xq = '//button[//div[text()="Log in"]]'
     continue_xq = '//button[text()="Continue"]'
     next_cq = 'prose'
     button_tq = 'button'
-    # next_xq     = '//button[//div[text()="Next"]]'
     done_xq = '//button[//div[text()="Done"]]'
-
     chatbox_cq = 'text-base'
     wait_cq = 'text-2xl'
     reset_xq = '//a[text()="New chat"]'
@@ -198,14 +124,12 @@ class Handler:
         """the conversation is refreshed"""
         self.browser.find_element(By.XPATH, self.reset_xq).click()
 
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("username")
+#     parser.add_argument("password")
+#     args = parser.parse_args()
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("username")
-    parser.add_argument("password")
-    args = parser.parse_args()
-
-    chatgpt = Handler(args.username, args.password)
-    result = chatgpt.interact("Hello, how are you today")
-    print(result)
+#     chatgpt = Handler(args.username, args.password)
+#     result = chatgpt.interact("Hello, how are you today")
+#     print(result)
